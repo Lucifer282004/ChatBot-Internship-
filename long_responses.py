bdc9ad57adf03d2f778b5bdb5f70cd9d2ce27ba8
Lucifer282004 <126413375+Lucifer282004@users.noreply.github.com>
2023-12-10 23:03:44 +0530

    long_responses.py

--- long_responses.py
+++ long_responses.py
@@ -1,7 +1,6 @@
 import random
 
-R_EATING = "I don't like eating anything because I'm a bot obviously!"
-R_ADVICE = "I can Do 3 main operatios like help you for leave application along with the CL pl and i can also raise tickets for certain things"
+R_ADVICE = "I can Do 2 main operations like help you for leave application along with the CL pl and i can also raise tickets for certain things"
 
 
 def unknown():
